3e74e3a81009f140c09b6638fb49f21d8fd9ec2f
BriciWorld <30957824+bw-tech@users.noreply.github.com>
2018-01-06 17:26:16 +0200

    The user's avatar is now in a thumbnail. Now with ID!

--- Commands/profile.js
+++ Commands/profile.js
@@ -1,27 +1,31 @@
 exports.run = (client, msg, args) => {
     msg.channel.send({embed: {
-        color: 194596,
+        color: 0x194596,
         author: {
           name: client.user.username,
           icon_url: client.user.avatarURL
         },
         description: `Information for **${msg.author.username}**`,
+        thumbnail: {
+          url: msg.author.avatarURL
+           },
           fields: [{
             name: ":hash: Discord Tag",
             value: `${msg.author.tag}`
           },
           {
             name: ":calendar_spiral: Account created",
-            value: `${msg.author.createdAt}`
+            value: `${msg.author.createdAt}`,
+            inline: true
+          },
+          {
+            name: ":id:",
+            value: `${msg.author.id}`,
+            inline: true
           },
         ],
+        timestamp: new Date(),
         footer: {
           icon_url: msg.author.avatarURL,
-          text: `User's avatar`
+          text: `Requested by ${msg.author.tag}`
             }
-        }
-    });
-    console.log(`[info] Treating ${msg.content} by ${msg.author.tag} from ${msg.guild} as a command.`);
-    };
-      config: {}
-    
