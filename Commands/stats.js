dff909f54651d17e7971c2e7816af44a25d47ed2
BriciWorld <30957824+bw-tech@users.noreply.github.com>
2018-01-06 17:29:32 +0200

    Edited stats command

--- Commands/stats.js
+++ Commands/stats.js
@@ -1,36 +1,42 @@
 exports.run = (client, msg, args) => {
     msg.channel.send({embed: {
-      color: 194596,
+      color: 0x194596,
       author: {
         name: client.user.username + " stats",
         icon_url: client.user.avatarURL
       },
       fields: [{
         name: "Guilds",
-        value: `${client.guilds.size}`
+        value: `${client.guilds.size}`,
+        inline: true
       },
       {
         name: "Users",
-        value: `${client.users.size}`
+        value: `${client.users.size}`,
+        inline: true
       },
       {
         name: "Channels",
-        value: `${client.channels.size}`
+        value: `${client.channels.size}`,
+        inline: true
       },
       {
         name: "Shards",
-        value: `1`
+        value: `1`,
+        inline: true
       },
       {
         name: "Bot Version",
-        value: `v1.2`
+        value: `v1.3`,
+        inline: true
       },
     ],
-      footer: {
-          icon_url: msg.author.avatarURL,
-          text: `Sent by ${msg.author.tag} at ${msg.createdAt}`
-            }
+    timestamp: new Date(),
+    footer: {
+      icon_url: msg.author.avatarURL,
+      text: `Requested by ${msg.author.tag}`
         }
+                }
   });
   console.log(`[info] Treating ${msg.content} by ${msg.author.tag} from ${msg.guild} as a command.`);
 };
