15389fe5721747fc9201d43f7a56016fad123c03
BriciWorld <30957824+bw-tech@users.noreply.github.com>
2018-01-06 17:07:37 +0200

    Edited credits command

--- Commands/credits.js
+++ Commands/credits.js
@@ -1,6 +1,6 @@
 exports.run = (client, msg, args) => {
 msg.channel.send({embed: {
-    color: 194596,
+    color: 0x194596,
     author: {
       name: client.user.username + " Credits",
       icon_url: client.user.avatarURL
@@ -8,12 +8,13 @@ msg.channel.send({embed: {
     description: "Author of the bot: **BriciWorld#1434**",
       fields: [{
         name: ":tools: Contribuitors",
-        value: "There are no contribuitors for this bot. If you want to be on this list just contribute to this bot."
+        value: "There are no contribuitors for this bot. If you want to be on this list just contribute to this bot on GitHub."
       },
     ],
+    timestamp: new Date(),
     footer: {
       icon_url: msg.author.avatarURL,
-      text: `Sent by ${msg.author.tag} at ${msg.createdAt}`
+      text: `Requested by ${msg.author.tag}`
         }
     }
 });
