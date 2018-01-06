bfe2968e26936cd8c670c7243778047dc9e0d5f8
BriciWorld <30957824+bw-tech@users.noreply.github.com>
2018-01-06 17:30:11 +0200

    Update year.js

--- Commands/year.js
+++ Commands/year.js
@@ -1,5 +1,5 @@
 exports.run = (client, msg, args) => {
-    msg.channel.send(`:calendar_spiral: 4 weeks left until 2018!`);
+    msg.channel.send(`:calendar_spiral: 12 months left until 2019!`);
     console.log(`[info] Treating ${msg.content} by ${msg.author.tag} from ${msg.guild} as a command.`);
     };
   config: {}
