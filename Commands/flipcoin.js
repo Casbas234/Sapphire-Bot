178accf83666dd3c480e4ca7baa5824c264a3ba2
BriciWorld <30957824+bw-tech@users.noreply.github.com>
2018-01-06 17:14:54 +0200

    Command now also flips Heads

--- Commands/flipcoin.js
+++ Commands/flipcoin.js
@@ -1,5 +1,9 @@
 exports.run = (client, msg, args) => {
-    msg.channel.send(`:white_check_mark: You flipped **${coin}**.`);
+    var testthings = [
+        `Heads`, `Tails`
+    ]
+    var coc = testthings[Math.floor(Math.random() * testthings.length)];
+    msg.channel.send(`:white_check_mark: You flipped **${coc.toString()}**.`);
     console.log(`[info] Treating ${msg.content} by ${msg.author.tag} from ${msg.guild} as a command.`);
 };
   config: {}
