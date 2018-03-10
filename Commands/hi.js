bd30d89a07e6388a5a430c793c0742866b9f68d5
BriciWorld <30957824+bw-tech@users.noreply.github.com>
2018-03-10 14:38:14 +0200

    Update hi.js

--- Commands/hi.js
+++ Commands/hi.js
@@ -1,6 +1,15 @@
 exports.run = (client, msg, args) => {
-    msg.channel.send(`Hi there!
-Type s>help for commands!`);
-    console.log(`[info] Treating ${msg.content} by ${msg.author.tag} from ${msg.guild} as a command.`);
+    msg.channel.send({embed: {
+      color: 0x194596,
+      title: `:wave: Oh hi?`,
+      description: `Hello **${msg.author.tag}**!`,
+      timestamp: new Date(),
+      footer: {
+        icon_url: msg.author.avatarURL,
+        text: `Requested by ${msg.author.tag}`
+          }
+        }
+  });
+  console.log(`[info] Treating ${msg.content} by ${msg.author.tag} from ${msg.guild} as a command.`);
 };
-  config: {}
+config: {}
