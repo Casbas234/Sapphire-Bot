5d5beaf3776a5e6e0ef517724a87f8783118e756
BriciWorld <30957824+bw-tech@users.noreply.github.com>
2018-03-10 14:42:52 +0200

    Update setstream.js

--- Commands/setstream.js
+++ Commands/setstream.js
@@ -2,8 +2,8 @@ exports.run = (client, msg, args) => {
 
     let game = args.join(" ")
 
-if(msg.author.id !== "327864603788967948") return msg.channel.send(`:x: **Are you stupid?** Only bot developers can use this command.`);
-client.user.setActivity(`${game}`, { type: 'STREAMING', url: `https://twitch.tv/briciworld`})
+if(msg.author.id !== "") return msg.channel.send(`:x: **Are you stupid?** Only bot developers can use this command.`);
+client.user.setActivity(`${game}`, { type: 'STREAMING', url: `twitch url`})
 msg.channel.send(`:white_check_mark: Successfully set the Streaming status to **${game}**.`)
  console.log(`[info] Treating ${msg.content} by ${msg.author.tag} from ${msg.guild} as a command.`);
 }
